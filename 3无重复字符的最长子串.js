1cfff881b748cda297e386f924b70fdee5c34da4
yululiang <yululiangj@163>
2019-03-13 16:18:49 +0800

    add

--- 3无重复字符的最长子串.js
+++ 3无重复字符的最长子串.js
@@ -1,1 +1,60 @@
-console.log('test');
+/**
+ * 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+
+示例 1:
+
+输入: "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+
+输入: "bbbbb"
+输出: 1
+解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+示例 3:
+
+输入: "pwwkew"
+输出: 3
+解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+ */
+
+function main(s) {
+
+    let result = '';
+
+    let thisMaxString = (s) => {
+
+        let rs = '';
+        let map = new Map();
+
+        for (let i=0; i<s.length; i++) {
+            if (map.get(s[i]) == null) {
+                map.set(s[i], i);
+                rs += s[i];
+            } else {
+                return rs;
+            }
+        }
+
+        return rs;
+    }
+
+    for (let i=0; i<s.length; i++) {
+        let tempResult = thisMaxString(s.slice(i));
+        
+        if (tempResult.length > result.length) {
+            result = tempResult;
+        }
+
+    }
+
+    return result.length;
+
+}
+//abcabcbb 3
+//bbbbb 1
+//pwwkew 3
+console.log(main('pwwkew'));
+
+console.log('aaa'.indexOf('a'));
---
+++ 4. 寻找两个有序数组的中位数.js
@@ -0,0 +1,36 @@
+// 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+
+// 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+
+// 你可以假设 nums1 和 nums2 不会同时为空。
+
+// 示例 1:
+
+// nums1 = [1, 3]
+// nums2 = [2]
+
+// 则中位数是 2.0
+// 示例 2:
+
+// nums1 = [1, 2]
+// nums2 = [3, 4]
+
+// 则中位数是 (2 + 3)/2 = 2.5
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let nums = nums1.concat(nums2).sort( (a, b) => { 
+         if (a>b) return 1;
+         if (a<b) return -1;
+         if (a===b) return 0;
+     });
+     if (nums.length%2===0) {
+         return (nums[nums.length/2-1] + nums[nums.length/2])/2;
+     } else {
+         return nums[Math.ceil(nums.length/2-1)];
+     }
+ };
